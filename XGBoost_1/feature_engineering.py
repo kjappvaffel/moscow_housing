cc07a171d151338ea8ca7048c832a5f9ee601c23
eirikjd <eirikdal@gmail.com>
2021-10-25 16:31:37 +0200

    XGBoost er bra, men mangler hyperparameteroptimalisering

--- XGBoost_1/feature_engineering.py
+++ XGBoost_1/feature_engineering.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
 def add_radius(df):
     #adds radius column to dataframe
@@ -29,3 +30,66 @@ def group_by_feature_and_price(df, feature, data_train = pd.DataFrame()):
 
         df[column_name] = df_merged['price']
     return df
+
+def one_hot_encode(df, column_to_encode):
+    """one hots encode for one singel column"""
+    encoded_df = pd.get_dummies(df[[column_to_encode]].astype(str))
+    df = pd.concat([df,encoded_df],axis=1)
+    return df
+
+def one_hot_encode_multiple(df, list_of_columns):
+    """takes in multiple columns and runs one hot encode for each column"""
+    for column_to_encode in list_of_columns:
+        #print(column_to_encode)
+        df = one_hot_encode(df, column_to_encode)
+    return df
+
+
+def add_retning(df):
+    """adds direction to dataframe, can be one of eight categories (N,S,W,E)"""
+    #straight up (north)
+    normal_vector = np.array([0,1])
+    #normal_vector = np.tile(normal_vector,(df.shape[0],1))
+    #normal_vector = normal_vector.reshape((2,-df.shape[0]))
+    temp = pd.DataFrame()
+    temp['latitude'] = df['latitude']-55.75
+    temp['longitude'] = df['longitude']-37.56
+
+
+    apartment_vector = temp[['latitude','longitude']].to_numpy()
+
+
+    #print(np.shape(apartment_vector), np.shape(normal_vector))
+    angles = []
+    for vector in apartment_vector:
+        if vector[0] < 0:
+            temp_angle = -angle_between(vector,normal_vector)
+        else:
+            temp_angle = angle_between(vector,normal_vector)
+        angles.append(temp_angle)
+
+    angles = [element * 10 for element in angles]
+
+
+    angles_series = pd.Series(np.array(angles))
+    angles_series.plot.hist()
+
+    df['direction'] = angles_series
+    max = df.direction.max().round()
+    min = df.direction.min().round()
+    bins = [min,min*7/8,min*5/8,min*3/8,min/8,max/8,max*3/8,max*5/8,max*7/8,max]
+    rounded_bins = [element.round() for element in bins]
+    #print(rounded_bins)
+    direction = pd.cut(df.direction, bins= rounded_bins,labels=['S','SW','W','NW','N','NE','E','SE','S'],ordered=False)
+    df['direction'] = direction
+    return df
+
+def unit_vector(vector):
+    """returns the unit vector if the vector"""
+    return vector/np.linalg.norm(vector)
+
+def angle_between(v1,v2):
+    """returns angle between two vectors in radian"""
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u,v2_u),-1,1))
